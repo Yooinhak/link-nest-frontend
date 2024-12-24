e7081d06e96d87a63aa82b374065b79a821fcc91
Yooinhak <syu3236@gmail.com>
2024-12-24 15:46:42 +0900

    fix. pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata, Viewport } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import Head from 'next/head';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -16,9 +15,23 @@ export const viewport: Viewport = {
   themeColor: '#222',
 };
 
-export const metadata: Metadata = {
-  title: 'link-nest',
+export const metadata = {
+  title: 'Link Nest',
   description: '즐겨찾는 공유 링크를 모두 모아둘 수 있는 아늑한 장소입니다.',
+  openGraph: {
+    title: 'Link Nest',
+    description: '즐겨찾는 공유 링크를 모두 모아둘 수 있는 아늑한 장소입니다.',
+    url: '/',
+    siteName: 'Link Nest',
+    images: [
+      {
+        url: '/icons/ln_icon.png',
+        width: 192,
+        height: 192,
+        alt: 'Link Nest Icon',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -28,9 +41,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="manifest" href="/manifest.json" crossOrigin="use-credentials" />
-      </Head>
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#222" />
+        <meta name="description" content="즐겨찾는 공유 링크를 모두 모아둘 수 있는 아늑한 장소입니다." />
+        <link rel="icon" href="/icons/ln_icon.png" sizes="192x192" />
+        <link rel="icon" href="/icons/ln_icon.png" sizes="512x512" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
     </html>
   );
