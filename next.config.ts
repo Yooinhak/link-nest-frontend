43137216b11e937cdd340d885992f74666bb25cb
Yooinhak <syu3236@gmail.com>
2024-12-27 18:43:17 +0900

    fix. pwa config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
-/** @type {import('next').NextConfig} */
-import withPWA from 'next-pwa';
-
-const nextPWA = withPWA({
-  dest: 'public',
+const withPWA = require('next-pwa')({
+  dest: 'public', // Service Worker 및 기타 PWA 파일이 생성될 위치
+  register: true, // Service Worker 등록
+  skipWaiting: true, // 업데이트 시 대기하지 않고 즉시 활성화
 });
 
-const nextConfig = {};
-
-export default nextPWA(nextConfig);
+module.exports = withPWA({
+  // 다른 Next.js 설정
+});
