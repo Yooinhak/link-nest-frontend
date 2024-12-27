d6b654790600bff4bfb30c925e5449d0e67ce921
Yooinhak <syu3236@gmail.com>
2024-12-27 18:26:49 +0900

    feat. tailwind css install

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Select from '@components/Select';
 
 export default function Home() {
   return (
-    <div>
+    <div className="w-full p-12 bg-slate-400">
       <Select />
     </div>
   );
