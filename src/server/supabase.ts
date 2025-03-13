1175391df53a544f62b4e764f69628c135d73ee3
Yooinhak <syu3236@gmail.com>
2025-03-13 11:29:54 +0900

    feat. 회원가입 및 로그인 테스트 with supabase

--- src/server/supabase.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-import { Database } from './type';
-
-const SUPABASE_URL = process.env.SUPABASE_PROJECT_URL as string;
-const SUPABASE_ANON_KEY = process.env.SUPABASE_API_KEY as string;
-
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
