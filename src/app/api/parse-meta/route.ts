73c9f10fe10beecccff0738cb1a8212c3a2eeee9
Yooinhak <syu3236@gmail.com>
2025-03-18 10:01:06 +0900

    feat. scrapping library change (cheerio -> puppeteer)

--- src/app/api/parse-meta/route.ts
+++ src/app/api/parse-meta/route.ts
@@ -1,5 +1,5 @@
-import * as cheerio from 'cheerio';
 import { NextRequest, NextResponse } from 'next/server';
+import * as puppeteer from 'puppeteer';
 
 interface Metadata {
   title: string | null;
@@ -16,39 +16,43 @@ export async function GET(request: NextRequest): Promise<NextResponse> {
     return NextResponse.json({ error: 'URL is required' }, { status: 400 });
   }
 
-  // URL 유효성 검사
-  if (!/^https?:\/\/[^ "]+$/.test(url)) {
-    return NextResponse.json({ error: 'Invalid URL' }, { status: 400 });
-  }
-
   // 캐시 확인
   if (cache.has(url)) {
     return NextResponse.json(cache.get(url)!);
   }
 
   try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      console.error(`Failed to fetch the URL. Status: ${response.status}`);
-      throw new Error('Failed to fetch the URL');
-    }
+    const browser = await puppeteer.launch({ headless: 'shell' });
+    const page = await browser.newPage();
 
-    const html = await response.text();
+    // 브라우저에서 요청하는 것처럼 보이도록 User-Agent 설정
+    await page.setUserAgent(
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    );
 
-    const $ = cheerio.load(html);
+    await page.goto(url, { waitUntil: 'domcontentloaded' });
 
-    const metadata: Metadata = {
-      title: $('meta[property="og:title"]').attr('content') || $('title').text() || null,
-      description: $('meta[property="og:description"]').attr('content') || null,
-      image: $('meta[property="og:image"]').attr('content') || null,
-    };
+    // 메타데이터 추출
+    const metadata: Metadata = await page.evaluate(() => {
+      return {
+        title: document.querySelector('meta[property="og:title"]')?.getAttribute('content') || document.title || null,
+        description:
+          document.querySelector('meta[property="og:description"]')?.getAttribute('content') ||
+          document.querySelector('meta[name="description"]')?.getAttribute('content') ||
+          null,
+        image:
+          document.querySelector('meta[property="og:image"]')?.getAttribute('content') ||
+          document.querySelector('meta[name="twitter:image"]')?.getAttribute('content') ||
+          null,
+      };
+    });
 
-    console.log('Extracted Metadata:', metadata);
+    await browser.close();
+
     cache.set(url, metadata);
-
     return NextResponse.json(metadata);
   } catch (error) {
-    console.error('Error in metadata fetching:', error);
+    console.error('Error fetching metadata:', error);
     return NextResponse.json({ error: 'Failed to fetch metadata' }, { status: 500 });
   }
 }
