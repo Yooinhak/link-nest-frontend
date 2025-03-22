a39574ad7cc14aef9cec91d69a1d4536a4fa1e39
Yooinhak <syu3236@gmail.com>
2025-03-22 11:25:27 +0900

    fix. prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,19 +1,14 @@
-module.exports ={
-    "singleQuote": true,
-    "parser": "typescript",
-    "semi": true,
-    "useTabs": false,
-    "tabWidth": 2,
-    "printWidth": 120,
-    "arrowParens": "always",
+module.exports = {
+  singleQuote: true,
+  parser: 'typescript',
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 120,
+  arrowParens: 'always',
 
-    "plugins": [require.resolve("@trivago/prettier-plugin-sort-imports")],
-    "importOrder": [
-        "^[a-z]",
-        "^@components/(.*)$",
-        "^@hooks/(.*)$",
-        "^\\.{1,2}/"
-    ],
-    "importOrderSeparation": true,
-    "importOrderSortSpecifiers": true
-}
+  plugins: [require.resolve('@trivago/prettier-plugin-sort-imports')],
+  importOrder: ['^(react|next)', '<THIRD_PARTY_MODULES>', '^(components|utils)(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
