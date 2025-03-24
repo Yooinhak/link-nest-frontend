2615001ed8aa624aa8f2ba5f06eb2f19900db05e
Yooinhak <syu3236@gmail.com>
2025-03-24 16:26:23 +0900

    fix. middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,20 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
 
-export function middleware(req: NextRequest) {
-  const url = req.nextUrl;
+import { updateSession } from '@utils/supabase/middleware';
 
-  // Supabase 인증 토큰 가져오기
-  const tokenKey = process.env.NEXT_PUBLIC_SUPABASE_TOKEN;
-  const supabaseToken = tokenKey ? req.cookies.get(tokenKey) : undefined;
-
-  const hasToken = !!supabaseToken; // 값이 있으면 true, 없으면 false
-
-  if (!url.pathname.startsWith('/auth') && !hasToken) {
-    return NextResponse.redirect(new URL('/auth/login', req.url));
-  }
-
-  if ((url.pathname.startsWith('/auth') || url.pathname === '/') && hasToken) {
-    return NextResponse.redirect(new URL('/home', req.url));
-  }
-
-  return NextResponse.next();
+export async function middleware(req: NextRequest) {
+  return await updateSession(req);
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|manifest.json|icons).*)'],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
 };
