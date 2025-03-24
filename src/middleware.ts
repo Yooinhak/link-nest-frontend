ef62d15c95ddb90adf76ab604e6f62258e7e90c8
Yooinhak <syu3236@gmail.com>
2025-03-24 16:09:57 +0900

    fix. middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,14 +3,11 @@ import { NextRequest, NextResponse } from 'next/server';
 export function middleware(req: NextRequest) {
   const url = req.nextUrl;
 
-  // Supabase 인증 토큰 찾기 (code-verifier 제외)
-  const supabaseToken = req.cookies
-    .getAll()
-    .find(
-      ({ name }) => name.startsWith(`${process.env.NEXT_PUBLIC_SUPABASE_TOKEN}`) && !name.includes('-code-verifier'),
-    );
+  // Supabase 인증 토큰 가져오기
+  const tokenKey = process.env.NEXT_PUBLIC_SUPABASE_TOKEN;
+  const supabaseToken = tokenKey ? req.cookies.get(tokenKey) : undefined;
 
-  const hasToken = supabaseToken !== undefined;
+  const hasToken = !!supabaseToken; // 값이 있으면 true, 없으면 false
 
   if (!url.pathname.startsWith('/auth') && !hasToken) {
     return NextResponse.redirect(new URL('/auth/login', req.url));
@@ -24,14 +21,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-     */
-    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|manifest.json|icons).*)',
-  ],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|manifest.json|icons).*)'],
 };
