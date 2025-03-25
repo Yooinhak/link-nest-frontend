e93c2677f18bddd2f11f1ce924b7fd877358f5c6
Yooinhak <syu3236@gmail.com>
2025-03-25 15:40:59 +0900

    feat. tanstack query test

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -1,23 +1,35 @@
+'use client';
+
 import Image from 'next/image';
 
 import { SignOutButton } from '@components/Button';
-import { createClient } from '@utils/supabase/server';
+import { useQuery } from '@tanstack/react-query';
+import { createClient } from '@utils/supabase/component';
 
-export default async function ProfilePage() {
-  const supabase = await createClient();
+export default function ProfilePage() {
+  const supabase = createClient();
   const {
-    data: { user },
-  } = await supabase.auth.getUser();
+    data: user,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ['userProfile'],
+    queryFn: async () => await supabase.auth.getUser(),
+    select: data => data.data.user,
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>유저 정보를 불러오지 못했습니다.</p>;
 
   return (
     <div>
       <div>{user?.email}</div>
       <Image
-        src={user?.user_metadata?.avatar_url}
+        src={user?.user_metadata?.avatar_url || ''}
         width={64}
         height={64}
         className="rounded-full"
-        alt={'user_avatar_image'}
+        alt="user_avatar_image"
       />
       <SignOutButton />
     </div>
