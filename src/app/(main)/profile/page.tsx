ce8a4a8ea2840ef9eb44c60279b7e629e35ab296
Yooinhak <syu3236@gmail.com>
2025-03-26 04:00:03 +0900

    feat. shadcn skeleton component

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -3,34 +3,34 @@
 import Image from 'next/image';
 
 import { SignOutButton } from '@components/Button';
+import { Skeleton } from '@components/Skeleton';
 import { useQuery } from '@tanstack/react-query';
 import { createClient } from '@utils/supabase/component';
 
 export default function ProfilePage() {
   const supabase = createClient();
-  const {
-    data: user,
-    isLoading,
-    isError,
-  } = useQuery({
+  const { data: user, isLoading } = useQuery({
     queryKey: ['userProfile'],
     queryFn: async () => await supabase.auth.getUser(),
     select: data => data.data.user,
   });
 
-  if (isLoading) return <p>Loading...</p>;
-  if (isError) return <p>유저 정보를 불러오지 못했습니다.</p>;
-
   return (
-    <div>
+    <div className="p-4 flex flex-col items-center">
+      {isLoading ? (
+        <Skeleton className="w-[128px] h-[128px] rounded-full" />
+      ) : (
+        <Image
+          src={user?.user_metadata?.avatar_url || ''}
+          width={128}
+          height={128}
+          className="rounded-full"
+          alt="user_avatar_image"
+        />
+      )}
+
       <div>{user?.email}</div>
-      <Image
-        src={user?.user_metadata?.avatar_url || ''}
-        width={64}
-        height={64}
-        className="rounded-full"
-        alt="user_avatar_image"
-      />
+
       <SignOutButton />
     </div>
   );
