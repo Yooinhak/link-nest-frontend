7d802b2a77ab26f07d5e8483fff96f7e685b41ce
Yooinhak <syu3236@gmail.com>
2025-03-27 13:34:37 +0900

    feat. Drawer component

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -1,3 +1,30 @@
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@components/Drawer';
+
 export default function HomePage() {
-  return <div>home!</div>;
+  return (
+    <Drawer>
+      <DrawerTrigger>Open</DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>폴더 생성</DrawerTitle>
+          <DrawerDescription>nest-link를 한곳에 모아서 볼 수 있어요!</DrawerDescription>
+        </DrawerHeader>
+
+        <DrawerClose>
+          <div>test123</div>
+        </DrawerClose>
+
+        <DrawerFooter></DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
 }
