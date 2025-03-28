7f723d90261333145489ebdb8ac7514348beb5e5
Yooinhak <syu3236@gmail.com>
2025-03-28 17:41:13 +0900

    feat. input component

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -8,6 +8,7 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from '@components/Drawer';
+import { OriginInput } from '@components/Input';
 
 export default function HomePage() {
   return (
@@ -19,8 +20,14 @@ export default function HomePage() {
           <DrawerDescription>nest-link를 한곳에 모아서 볼 수 있어요!</DrawerDescription>
         </DrawerHeader>
 
+        <div className="flex justify-center">
+          <div className="w-full max-w-[500px] p-4">
+            <OriginInput className="" />
+          </div>
+        </div>
+
         <DrawerClose>
-          <div>test123</div>
+          <div>닫기</div>
         </DrawerClose>
 
         <DrawerFooter></DrawerFooter>
