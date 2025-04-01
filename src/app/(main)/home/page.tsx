191205c99d732bce81e64a39bd1944046e6343d1
Yooinhak <syu3236@gmail.com>
2025-04-01 17:55:37 +0900

    feat. get folders api test

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -10,11 +10,16 @@ import {
   DrawerTrigger,
 } from '@components/Drawer';
 import { OriginInput } from '@components/Input';
+import { createClient } from '@utils/supabase/server';
 
-export default function HomePage() {
+export default async function HomePage() {
+  const supabase = await createClient();
+  const { data } = await supabase.from('folders').select();
+
   return (
-    <div className="p-4">
+    <div className="p-4 flex flex-col gap-4">
       <CreateFolderButton />
+      {data?.map(d => <div key={d.id}>{d.name}</div>)}
     </div>
   );
 }
@@ -22,7 +27,7 @@ export default function HomePage() {
 const CreateFolderButton = () => {
   return (
     <Drawer>
-      <DrawerTrigger>
+      <DrawerTrigger asChild>
         <Button variant="outline">Open Drawer</Button>
       </DrawerTrigger>
       <DrawerContent>
