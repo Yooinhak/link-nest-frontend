0a1c959ed86dfa5562605268c2584225f3b2adeb
Yooinhak <syu3236@gmail.com>
2025-04-07 15:51:39 +0900

    feat. dropdown menu component

--- src/components/FolderList/imdex.tsx
+++ src/components/FolderList/imdex.tsx
@@ -1,8 +1,11 @@
 'use client';
 
+import { Button } from '@components/Button';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@components/DropdownMenu';
 import { useQuery } from '@tanstack/react-query';
 import { queryKeys } from '@utils/react-query/queryKeys';
 import { createClient } from '@utils/supabase/component';
+import { MoreHorizontal, Pen, Trash2 } from 'lucide-react';
 
 const FolderList = () => {
   const supabase = createClient();
@@ -20,9 +23,26 @@ const FolderList = () => {
       {folderList.map(folder => (
         <li
           key={folder.id}
-          className="flex w-full flex-col items-start justify-between rounded-md border px-4 py-2 sm:flex-row sm:items-center cursor-pointer bg-white"
+          className="flex items-center justify-between rounded-md border px-4 py-2 cursor-pointer bg-white"
         >
           <span className="text-muted-foreground">{folder.name}</span>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" size="sm" className="px-2">
+                <MoreHorizontal />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent side="left" align="start">
+              <DropdownMenuItem onClick={() => console.log('수정하기')}>
+                <Pen />
+                <span>수정</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Trash2 />
+                <span>삭제</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </li>
       ))}
     </ul>
