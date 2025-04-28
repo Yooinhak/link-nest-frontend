3526f39c19b336a0cec348dd8267ad3358889b50
Yooinhak <syu3236@gmail.com>
2025-04-28 15:19:36 +0900

    fix. build error

--- src/app/(main)/folder/[folderId]/page.tsx
+++ src/app/(main)/folder/[folderId]/page.tsx
@@ -2,10 +2,11 @@ import { FolderDetailHeader } from '@components/Header';
 import PostList from '@components/PostList';
 import { createClient } from '@utils/supabase/server';
 
-export default async function FolderDetailPage({ params }: { params: { folderId: string } }) {
+export default async function FolderDetailPage({ params }: { params: Promise<{ folderId: string }> }) {
+  const { folderId } = await params;
   const supabase = await createClient();
-  const { data: folder } = await supabase.from('folders').select().eq('id', Number(params.folderId)).single();
-  const { data: posts } = await supabase.from('posts').select().eq('folder_id', Number(params.folderId));
+  const { data: folder } = await supabase.from('folders').select().eq('id', Number(folderId)).single();
+  const { data: posts } = await supabase.from('posts').select().eq('folder_id', Number(folderId));
 
   return (
     <div>
