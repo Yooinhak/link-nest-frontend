7502861ce3cf6a4df2d95cddf1219d2916dba075
Yooinhak <syu3236@gmail.com>
2025-04-29 13:36:08 +0900

    feat. post loading fallback

--- src/app/(main)/folder/page.tsx
+++ src/app/(main)/folder/page.tsx
@@ -2,10 +2,11 @@ import { Suspense } from 'react';
 
 import { FolderDetailHeader } from '@components/Header';
 import PostList from '@components/PostList';
+import { PostPageLoading } from '@components/Skeleton';
 
 export default function FolderDetailPage() {
   return (
-    <Suspense>
+    <Suspense fallback={<PostPageLoading />}>
       <FolderDetailHeader />
       <PostList />
     </Suspense>
